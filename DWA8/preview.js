bb30408c2011ba0b0f2c4cb25d61d90bb83b5c23
Ato-Mothibi <atomothibi@gmail.com>
2023-06-23 06:20:23 +0200

    dwa

--- DWA8/preview.js
+++ DWA8/preview.js
@@ -17,36 +17,89 @@ import { authors } from './data.js';
 
 // export {createPreviewElemenent}
 
-// // Interfaces
-// class ElementCreator {
-//   createPreviewElement(book) {}
-// }
-// class PreviewDataProvider {
-//     getMatches(startIndex, endIndex, filters) {}
-//   }
+
+
+// Interfaces
+class ElementCreator {
+    createPreviewElement(book) {}
+  }
   
-// class PreviewRenderer {
-//    renderPreviewItems(matches) {}
-//  }
+  class PreviewDataProvider {
+    getMatches(startIndex, endIndex, filters) {}
+  }
+  
+  class PreviewRenderer {
+    renderPreviewItems(matches) {}
+  }
+  
+  // DOM-related classes implementing interfaces
+  class DOMElementCreator extends ElementCreator {
+    createPreviewElement(book) {
+      const element = document.createElement('button');
+      element.classList = 'preview';
+      element.setAttribute('data-preview', book.id);
+      element.innerHTML = `
+          <img
+              class="preview__image"
+              src="${book.image}"
+          />
+          <div class="preview__info">
+              <h3 class="preview__title">${book.title}</h3>
+              <div class="preview__author">${authors[book.author]}</div>
+          </div>
+      `;
+      return element;
+    }
+  }
+  
+  class PreviewDataFilter {
+    static filterMatches(books, filters) {
+      const { genre, author } = filters;
+      let filteredBooks = books;
+  
+      if (genre !== 'any') {
+        filteredBooks = filteredBooks.filter((book) => book.genre === genre);
+      }
+  
+      if (author !== 'any') {
+        filteredBooks = filteredBooks.filter((book) => book.author === author);
+      }
+  
+      return filteredBooks;
+    }
+  }
+  
+  class PreviewDataProviderImpl extends PreviewDataProvider {
+    getMatches(startIndex, endIndex, filters) {
+      const filteredMatches = PreviewDataFilter.filterMatches(books, filters);
+      const matches = filteredMatches.slice(startIndex, endIndex);
+      return matches;
+    }
+  }
+  
+  class DOMPreviewRenderer extends PreviewRenderer {
+    constructor(elementCreator) {
+      super();
+      this.elementCreator = elementCreator;
+    }
+  
+    renderPreviewItems(matches) {
+      const fragment = document.createDocumentFragment();
+      for (const book of matches) {
+        const element = this.elementCreator.createPreviewElement(book);
+        fragment.appendChild(element);
+      }
+      document.querySelector('[data-list-items]').innerHTML = '';
+      document.querySelector('[data-list-items]').appendChild(fragment);
+    }
+  }  
 
-// // DOM-related classes implementing interfaces
-// class DOMElementCreator extends ElementCreator {
-//   createPreviewElement(book) {
-//     const element = document.createElement('button');
-//     element.classList = 'preview';
-//     element.setAttribute('data-preview', book.id);
-//     element.innerHTML = `
-//         <img
-//             class="preview__image"
-//             src="${book.image}"
-//         />
-//         <div class="preview__info">
-//             <h3 class="preview__title">${book.title}</h3>
-//             <div class="preview__author">${authors[book.author]}</div>
-//         </div>
-//     `;
-//     return element;
+//   function createBookApp() {
+//     const elementCreator = new DOMElementCreator();
+//     const previewDataProvider = new PreviewDataProviderImpl();
+//     const previewRenderer = new DOMPreviewRenderer(elementCreator);
+  
+//     return {elementCreator, previewDataProvider, previewRenderer};
 //   }
-// }
 
-// export {DOMElementCreator, ElementCreator, PreviewDataProvider, PreviewRenderer}
+  export { DOMElementCreator, PreviewDataProviderImpl, DOMPreviewRenderer}
--- DWA8/scripts.js
+++ DWA8/scripts.js
@@ -1,17 +1,21 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js';
+import { DOMElementCreator, PreviewDataProviderImpl, DOMPreviewRenderer} from './preview.js';
+// import createBookApp from './preview'
+// // Interfaces
+// class ElementCreator {
+//   createPreviewElement(book) {}
+// }
 
-// Interfaces
-class ElementCreator {
-  createPreviewElement(book) {}
-}
 
-class PreviewDataProvider {
-  getMatches(startIndex, endIndex, filters) {}
-}
+// class PreviewDataProvider {
+//   getMatches(startIndex, endIndex, filters) {}
+// }
 
-class PreviewRenderer {
-  renderPreviewItems(matches) {}
-}
+
+// class PreviewRenderer {
+//   renderPreviewItems(matches) {}
+// }
+
 
 //Classes and it's methods
 class BookApp {
@@ -27,6 +31,7 @@ class BookApp {
     this.previewRenderer = previewRenderer;
   }
 
+
   populatePreviewItems(startIndex, endIndex) {
     const fragment = document.createDocumentFragment();
     for (const book of this.matches.slice(startIndex, endIndex)) {
@@ -36,11 +41,13 @@ class BookApp {
     return fragment;
   }
 
+
   closePreview() {
     document.querySelector('[data-list-close]').addEventListener('click', (event) => {
       document.querySelector('[data-list-active]').open = false;
     });
   }
+
 
   populateGenres() {
     const genreHtml = document.createDocumentFragment();
@@ -57,6 +64,7 @@ class BookApp {
     return genreHtml;
   }
 
+
   populateAuthors() {
     const authorsHtml = document.createDocumentFragment();
     const firstAuthorElement = document.createElement('option');
@@ -72,11 +80,13 @@ class BookApp {
     return authorsHtml;
   }
 
+
   setSearchCancelButton() {
     document.querySelector('[data-search-cancel]').addEventListener('click', () => {
       document.querySelector('[data-search-overlay]').open = false;
     });
   }
+
 
   setSearchButton() {
     document.querySelector('[data-header-search]').addEventListener('click', () => {
@@ -84,6 +94,7 @@ class BookApp {
       document.querySelector('[data-search-title]').focus();
     });
   }
+
 
   setSearchForm() {
     document.querySelector('[data-search-form]').addEventListener('submit', (event) => {
@@ -124,6 +135,7 @@ class BookApp {
     });
   }
 
+
   setTheme(theme) {
     const isDarkMode = theme === 'night';
     document.querySelector('[data-settings-theme]').value = theme;
@@ -131,17 +143,20 @@ class BookApp {
     document.documentElement.style.setProperty('--color-light', isDarkMode ? '10, 10, 20' : '255, 255, 255');
   }
 
+
   setSettingsCancelButton() {
     document.querySelector('[data-settings-cancel]').addEventListener('click', () => {
       document.querySelector('[data-settings-overlay]').open = false;
     });
   }
 
+
   setSettingsButton() {
     document.querySelector('[data-header-settings]').addEventListener('click', () => {
       document.querySelector('[data-settings-overlay]').open = true;
     });
   }
+
 
   setSettingsForm() {
     document.querySelector('[data-settings-form]').addEventListener('submit', (event) => {
@@ -152,6 +167,7 @@ class BookApp {
       document.querySelector('[data-settings-overlay]').open = false;
     });
   }
+
 
   setListButton() {
     document.querySelector('[data-list-button]').addEventListener('click', () => {
@@ -165,6 +181,7 @@ class BookApp {
     this.updateListButtonRemaining();
   }
 
+
   updateListButtonRemaining() {
     const remaining = Math.max(this.matches.length - (this.page * BOOKS_PER_PAGE), 0);
     const listButton = document.querySelector('[data-list-button]');
@@ -174,6 +191,7 @@ class BookApp {
     `;
     listButton.disabled = remaining === 0;
   }
+
 
   setListItemsClick() {
     document.querySelector('[data-list-items]').addEventListener('click', (event) => {
@@ -201,6 +219,7 @@ class BookApp {
     });
   }
 
+
   initializeApp() {
     const startingItems = this.populatePreviewItems(0, BOOKS_PER_PAGE);
     const genreHtml = this.populateGenres();
@@ -225,72 +244,90 @@ class BookApp {
   }
 }
 
-// DOM-related classes implementing interfaces
-class DOMElementCreator extends ElementCreator {
-  createPreviewElement(book) {
-    const element = document.createElement('button');
-    element.classList = 'preview';
-    element.setAttribute('data-preview', book.id);
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${book.image}"
-        />
-        <div class="preview__info">
-            <h3 class="preview__title">${book.title}</h3>
-            <div class="preview__author">${authors[book.author]}</div>
-        </div>
-    `;
-    return element;
-  }
-}
+
+// // DOM-related classes implementing interfaces
+// class DOMElementCreator extends ElementCreator {
+//   createPreviewElement(book) {
+//     const element = document.createElement('button');
+//     element.classList = 'preview';
+//     element.setAttribute('data-preview', book.id);
+//     element.innerHTML = `
+//         <img
+//             class="preview__image"
+//             src="${book.image}"
+//         />
+//         <div class="preview__info">
+//             <h3 class="preview__title">${book.title}</h3>
+//             <div class="preview__author">${authors[book.author]}</div>
+//         </div>
+//     `;
+//     return element;
+//   }
+// }
+
 
 class PreviewDataFilter {
   static filterMatches(books, filters) {
     const { genre, author } = filters;
     let filteredBooks = books;
 
+
     if (genre !== 'any') {
       filteredBooks = filteredBooks.filter((book) => book.genre === genre);
     }
+
 
     if (author !== 'any') {
       filteredBooks = filteredBooks.filter((book) => book.author === author);
     }
 
+
     return filteredBooks;
   }
 }
 
-class PreviewDataProviderImpl extends PreviewDataProvider {
-  getMatches(startIndex, endIndex, filters) {
-    const filteredMatches = PreviewDataFilter.filterMatches(books, filters);
-    const matches = filteredMatches.slice(startIndex, endIndex);
-    return matches;
-  }
-}
 
-class DOMPreviewRenderer extends PreviewRenderer {
-  constructor(elementCreator) {
-    super();
-    this.elementCreator = elementCreator;
-  }
+// class PreviewDataProviderImpl extends PreviewDataProvider {
+//   getMatches(startIndex, endIndex, filters) {
+//     const filteredMatches = PreviewDataFilter.filterMatches(books, filters);
+//     const matches = filteredMatches.slice(startIndex, endIndex);
+//     return matches;
+//   }
+// }
 
-  renderPreviewItems(matches) {
-    const fragment = document.createDocumentFragment();
-    for (const book of matches) {
-      const element = this.elementCreator.createPreviewElement(book);
-      fragment.appendChild(element);
-    }
-    document.querySelector('[data-list-items]').innerHTML = '';
-    document.querySelector('[data-list-items]').appendChild(fragment);
-  }
-}
+
+// class DOMPreviewRenderer extends PreviewRenderer {
+//   constructor(elementCreator) {
+//     super();
+//     this.elementCreator = elementCreator;
+//   }
+
+
+//   renderPreviewItems(matches) {
+//     const fragment = document.createDocumentFragment();
+//     for (const book of matches) {
+//       const element = this.elementCreator.createPreviewElement(book);
+//       fragment.appendChild(element);
+//     }
+//     document.querySelector('[data-list-items]').innerHTML = '';
+//     document.querySelector('[data-list-items]').appendChild(fragment);
+//   }
+// }
+
+
+// const bookApp = createBookApp();
+// // Access the instances from the factory
+// const elementCreator = bookApp.elementCreator;
+// const previewDataProvider = bookApp.previewDataProvider;
+// const previewRenderer = bookApp.previewRenderer;
+
 
 // Usage of abstracted interfaces
 const elementCreator = new DOMElementCreator();
 const previewDataProvider = new PreviewDataProviderImpl();
 const previewRenderer = new DOMPreviewRenderer(elementCreator);
 
+
 const app = new BookApp(elementCreator, previewDataProvider, previewRenderer);
 app.initializeApp();
+
